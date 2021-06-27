6f4cfd785338f85fdd94a6d88f55a3c6373cf104
stsh1119 <stshlaptop@gmail.com>
2021-06-27 22:57:20 +0300

    Added Request model

--- request_trap/models.py
+++ request_trap/models.py
@@ -1,3 +1,18 @@
-from django.db import models
+from djongo import models
 
-# Create your models here.
+
+class Request(models.Model):
+    """Model, representing HTTP request with its main parts."""
+    request_id = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    remote_ip = models.CharField(max_length=20)
+    method = models.CharField(max_length=10)
+    scheme = models.CharField(max_length=5)
+    query_string = models.CharField(max_length=100)
+    query_params = models.CharField(max_length=100)
+    cookies = models.TextField()
+    headers = models.TextField()
+    body = models.TextField()
+
+    def __repr__(self):
+        return self.request_id
